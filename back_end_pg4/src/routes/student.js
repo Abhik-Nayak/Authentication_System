d4107d4a0c635c0541c32726781322a3e2782f59
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-11-12 08:57:49 +0530

    Setup

--- back_end_pg4/src/routes/student.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const path = require("../controllers/student")
-
-router.get ("/", path.getStudents);
-router.post("/",  path.addStudent);
-router.get ("/:id", path.getStudentById);
-router.post("/delete", path.deleteStudent);
-module.exports  = router;
